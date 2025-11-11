83aefdb6c5878cb2a215413eea55a48884323787
2465084 <ymg2465084@stu.o-hara.ac.jp>
2025-11-11 10:06:20 +0900

    11/11-sagara

---
+++ src/chapter3/HelloTest.java
@@ -0,0 +1,26 @@
+package chapter3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/hello" })
+public class HelloTest extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Hello!");
+		out.println(new java.util.Date());
+		out.println("Hello Wold");
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+}
