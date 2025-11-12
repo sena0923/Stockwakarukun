ee231a63ee2581548e57a127976ac5aa775ca031
sena0923 <ymg2465055@stu.o-hara.ac.jp>
2025-11-12 14:57:31 +0900

    1112

---
+++ src/Dao/CartDao.java
@@ -0,0 +1,52 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import bean.Cart;
+
+public class CartDao extends Dao {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+    }
+
+    public Cart get(String course_Id) throws Exception {
+
+        Cart Cart = null;
+        Connection connection = getConnection();
+        PreparedStatement statement = null;
+
+        try {
+            // 商品1件取得SQL
+            String sql = "SELECT * FROM goods WHERE goods_id = ?";
+
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, course_Id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                Cart = new Cart();
+
+                Cart.setCourse_id(resultSet.getString("course_id"));
+                Cart.setRd_id(resultSet.getString("rd_id"));
+                Cart.setGoods_id(resultSet.getString("goods_id"));
+            }
+
+        } catch (SQLException e) {
+            throw new Exception("データ取得エラー", e);
+        } finally {
+            if (statement != null) {
+                try { statement.close(); } catch (SQLException ignored) {}
+            }
+            if (connection != null) {
+                try { connection.close(); } catch (SQLException ignored) {}
+            }
+        }
+
+        return Cart;
+    }
+}
---
+++ src/Dao/Goods_AttributeDao.java
@@ -0,0 +1,52 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import bean.Goodsattribute;
+
+public class Goods_AttributeDao extends Dao {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+    }
+
+    public Goodsattribute get(String attribute_Id) throws Exception {
+
+        Goodsattribute goodsattribute = null;
+        Connection connection = getConnection();
+        PreparedStatement statement = null;
+
+        try {
+            // 商品1件取得SQL
+            String sql = "SELECT * FROM goods WHERE goods_id = ?";
+
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, attribute_Id); // ★修正：引数を使用
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                goodsattribute = new Goodsattribute();
+
+
+                goodsattribute.setCourse_id(resultSet.getString("course_id"));
+                goodsattribute.setCourse_name(resultSet.getString("course_name"));
+            }
+
+        } catch (SQLException e) {
+            throw new Exception("データ取得エラー", e);
+        } finally {
+            if (statement != null) {
+                try { statement.close(); } catch (SQLException ignored) {}
+            }
+            if (connection != null) {
+                try { connection.close(); } catch (SQLException ignored) {}
+            }
+        }
+
+        return goodsattribute;
+    }
+}
