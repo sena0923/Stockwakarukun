def97cf29219c0227b45b6cf5be6800f6b4f2f20
Tyogome <ymg2465054@stu.o-hara.ac.jp>
2026-01-13 10:59:49 +0900

    a

--- src/scoremanager/ECsite/CartListExecuteAction.java
+++ src/scoremanager/ECsite/CartListExecuteAction.java
@@ -1,47 +1,28 @@
 package scoremanager.ECsite;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import Dao.CartDao;
 import bean.Cart;
+import tool.Action;
 
-@WebServlet("/CartList.action")
-public class CartListExecuteAction extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+public class CartListExecuteAction extends Action {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        // セッションからユーザーID（rd_id）を取得
         String rdId = (String) request.getSession().getAttribute("rd_id");
 
+        CartDao dao = new CartDao();
+        List<Cart> cartList = dao.getCartList(rdId);
+        int totalPrice = dao.getTotalPrice(rdId);
 
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:h2:~/ecdb", "sa", "")) {
+        request.setAttribute("cartList", cartList);
+        request.setAttribute("totalPrice", totalPrice);
 
-            CartDao dao = new CartDao(conn);
-
-            List<Cart> cartList = dao.getCartList(rdId);
-            int totalPrice = dao.getTotalPrice(rdId);
-
-            request.setAttribute("cartList", cartList);
-            request.setAttribute("totalPrice", totalPrice);
-
-            request.getRequestDispatcher("../ECsite/cart.jsp").forward(request, response);
-
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
-
+        request.getRequestDispatcher("../ecSite/EClist.jsp").forward(request, response);
     }
 }
